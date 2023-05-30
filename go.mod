794b2a7c7079c8306f687f56fdf10d11e657c81f
lucifergotmad <amateur.fullstackdev@gmail.com>
2023-05-30 23:27:13 +0700

    feat:[major] add token password functionality on service

--- go.mod
+++ go.mod
@@ -14,11 +14,12 @@ require (
 )
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
-	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/crypto v0.9.0
 	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
--- pkg/user/service.go
+++ pkg/user/service.go
@@ -1,7 +1,22 @@
 package user
 
+import (
+	"os"
+
+	"github.com/dgrijalva/jwt-go"
+	"golang.org/x/crypto/bcrypt"
+)
+
 type IUserService interface {
 	GetUser(id uint64) (*User, error)
+	GenerateToken(user *User) (string, error)
+	SecurePassword(password string) (string, error)
+	CheckPasswordHash(password, hash string) (bool)
+}
+
+type Token struct {
+	userID uint64
+	jwt.StandardClaims
 }
 
 type UserService struct {
@@ -16,4 +31,21 @@ func NewService(repo IUserRepository) IUserService {
 
 func (s *UserService) GetUser(id uint64) (*User, error) {
 	return s.repo.GetUserByID(id)
+}
+
+func (s *UserService) GenerateToken(user *User) (string, error) {
+	secretKey := os.Getenv("JWT_SECRET_KEY")
+	payload := &Token{userID: user.id}
+	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), payload)
+	return token.SignedString([]byte(secretKey))
+}
+
+func (s *UserService) SecurePassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func (s *UserService) CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
